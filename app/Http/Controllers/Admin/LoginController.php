8e7616c1398dd4975b13c39ebe9928456282b063
ramay2088 <57621713+bilalswiftsolutions@users.noreply.github.com>
2023-02-09 15:30:22 +0500

    fixes

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
         	$saved_password = $check_email->password;
         	$given_password = $request->password;
 
-            dd($given_password );
+          
         	if(\Hash::check($given_password,$saved_password) == false)
         	{
         		return redirect()->back()->with('error', 'Password is wrong');
