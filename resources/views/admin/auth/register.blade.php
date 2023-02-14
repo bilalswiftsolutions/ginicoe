ec54e57d0d12291426a5d13ff6cdb5e83602f257
ramay2088 <57621713+bilalswiftsolutions@users.noreply.github.com>
2023-02-14 20:36:36 +0500

    fixes

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -37,7 +37,7 @@
                                 <div class="p-5">
 
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">SignUP</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">SignUp to Reduce Income Inequality</h1>
                                     </div>
 
                                     <form id="adminRegisterForm" action="{{ route('admin.register.store') }}"
