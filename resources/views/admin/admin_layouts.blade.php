4ff5e06dff8e5d07b28ae84fe5bf7e29cfe29c39
ramay2088 <57621713+bilalswiftsolutions@users.noreply.github.com>
2023-02-24 17:49:26 +0500

    fixes

--- resources/views/admin/admin_layouts.blade.php
+++ resources/views/admin/admin_layouts.blade.php
@@ -483,7 +483,7 @@
                 @yield('admin_content')
 
                 <div class="modal fade" id="chart_modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
+                    <div class="modal-dialog modal-xl">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="exampleModalLabel">Make Chart</h5>
