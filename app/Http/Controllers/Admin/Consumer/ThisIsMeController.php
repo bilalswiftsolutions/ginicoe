d96d067adf22556d5a9565e5e5ebc996c1b9679d
ramay2088 <57621713+bilalswiftsolutions@users.noreply.github.com>
2023-04-07 09:44:33 +0500

    fixes

--- app/Http/Controllers/Admin/Consumer/ThisIsMeController.php
+++ app/Http/Controllers/Admin/Consumer/ThisIsMeController.php
@@ -191,19 +191,7 @@ class ThisIsMeController extends Controller
                 )
             );
 
-            $message = "<pre>Thank you for signing up to become a Ginicoe Member.
-            We will perform some checking on our end before we can begin to protect your credit and debit card transaction.s
-            Please be patient.
-            If we require any additional info, we will contact you by mail.
-            
-            Ginicoe will never telephone you asking for information.
-            Ginicoe will never text you asking for information.
-            
-            Your data will never be sold.
-            
-            From all of our Ginicoe Associates, thank you once again for signing up.
-            We will email you once we are completed on our end.
-            Please be well.</pre>";
+            $message = "";
            
             Mail::to(session('email'))->send(new ConsumerThisIsMeMail('Thank you for signing up to become a Ginicoe Member', $message));
             return $attestation_info;
--- resources/views/emails/consumer_this_is_me.blade.php
+++ resources/views/emails/consumer_this_is_me.blade.php
@@ -1,3 +1,16 @@
 @component('mail::message')
-    {!! $message !!}
+    {{-- {!! $message !!} --}}
+    <pre>Thank you for signing up to become a Ginicoe Member.
+        We will perform some checking on our end before we can begin to protect your credit and debit card transaction.s
+        Please be patient.
+        If we require any additional info, we will contact you by mail.
+        
+        Ginicoe will never telephone you asking for information.
+        Ginicoe will never text you asking for information.
+        
+        Your data will never be sold.
+        
+        From all of our Ginicoe Associates, thank you once again for signing up.
+        We will email you once we are completed on our end.
+        Please be well.</pre>
 @endcomponent
