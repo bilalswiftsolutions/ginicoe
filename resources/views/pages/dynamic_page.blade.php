3d3cca8bd7c73a76017caa2f43ec1562f920ec61
ramay2088 <57621713+bilalswiftsolutions@users.noreply.github.com>
2023-04-17 15:34:46 +0500

    fixes

--- resources/views/pages/dynamic_page.blade.php
+++ resources/views/pages/dynamic_page.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="page-banner" style="background-image: url({{ asset('public/uploads/'.$dynamic_page_detail->dynamic_page_banner) }})">
+    <div class="page-banner"
+        style="background-image: url({{ asset('public/uploads/' . $dynamic_page_detail->dynamic_page_banner) }})">
         <div class="bg-page"></div>
         <div class="text">
             <h1>{{ $dynamic_page_detail->dynamic_page_name }}</h1>
@@ -14,36 +15,32 @@
         </div>
     </div>
 
-    <div @if($slug != 'donate') class="page-content" @endif>
+    <div @if ($slug != 'donate') class="page-content" @endif>
         <div class="container">
             <div class="row">
-                <div @if($slug == 'donate') class="col-md-12" @else class="col-md-12" @endif>
+                @if ($slug == 'donate')
+                    <div class="col-md-12">
+
+
+
+
+                        <div class=" mt_20 mb_20">
+                            <h5>Online Payment | Check | Trekker Volunteer</h5>
+
+                            <a href="https://donate.stripe.com/test_4gweVvdnda34bgQcMM" target="_blank"
+                                class="btn btn-primary">Click to Donate</a>
+
+
+                        </div>
+
+
+                    </div>
+                @endif
+                <div @if ($slug == 'donate') class="col-md-12" @else class="col-md-12" @endif>
                     {!! $dynamic_page_detail->dynamic_page_content !!}
                 </div>
-                @if($slug == 'donate')
-                <div  class="col-md-2">
-                    
-                    
-                      
-                       
-                        <div class=" mt_20">
-                            <h5>Online Payment | check | Trekker Volunteer</h5>
-    
-                           <a href="https://donate.stripe.com/test_4gweVvdnda34bgQcMM" target="_blank" class="btn btn-primary" >Click to Donate</a>
-                            @php
-                                $cents = 0;
-                            @endphp
-    
-                  
-                        </div>
-    
-                    
-                </div>
-                @endif
+
             </div>
         </div>
     </div>
-
-     
 @endsection
-
